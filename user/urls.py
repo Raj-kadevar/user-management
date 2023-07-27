d5b0997fc43e606cacbdb6f02a1ef74fb818b2d8
DivyeshP <divyesh.patoriya@trootech.com>
2023-07-27 16:28:56 +0530

    update and delete operation completed

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
 from django.urls import path
 
 from user import views
 
 urlpatterns = [
-    path('', views.BookView.as_view(), name="form"),
-    path('index/', views.show,name="index"),
+    path("", views.BookView.as_view(), name="add-user"),
+    path("index/", views.show, name="index"),
+    path("update/<int:id>", views.update, name="update"),
+    path("updater/<int:id>", views.updater, name="updater"),
+    path("delete/<int:id>", views.delete, name="delete"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import CreateView
 
 from user.form import Reg
@@ -29,3 +29,24 @@ class BookView(CreateView):
 def show(request):
     data = User.objects.all()
     return render(request, 'user/index.html', {"obj":data})
+def delete(request, id):
+    data = User.objects.get(id=id)
+    data.delete()
+    return redirect("index")
+
+def update(request, id):
+    get_user = User.objects.get(id=id)
+    return render(request,"user/update.html",{"key":get_user})
+
+def updater(request, id):
+    name = request.POST['name']
+    email = request.POST['email']
+    phone_num = request.POST['phone_number']
+    adrs = request.POST['address']
+    get_user = User.objects.get(id=id)
+    get_user.username = name
+    get_user.email = email
+    get_user.phone_number = phone_num
+    get_user.address = adrs
+    get_user.save()
+    return redirect("index")
