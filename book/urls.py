03650d034139b03a418a7c6444969fdbc52c7d0e
DivyeshP <divyesh.patoriya@trootech.com>
2023-08-02 16:59:45 +0530

    login feature implemented

--- book/urls.py
+++ book/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from book import views
 from user import views as user
 urlpatterns = [
-    path('',views.login, name="login"),
+    path('',views.MyView.as_view(), name="login"),
     path('index/',views.index,name="index"),
     path('registration_form/',user.UserView.as_view(),name="add-user"),
 ]
--- book/views.py
+++ book/views.py
@@ -1,17 +1,31 @@
 from django.contrib.auth import authenticate
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render,redirect
+from django.views import View
 
+login_status = 0
 
-def login(request):
-    return render(request,"book/login.html")
+# def login(request):
+#     return render(request,"book/login.html")
+
+class MyView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "book/login.html")
+    def post(self, request, *args, **kwargs):
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            global login_status
+            login_status = 1
+            return HttpResponseRedirect("index")
+        else:
+            error = "Username or password incorrect"
+            return render(request, "book/login.html", {"errors": error})
 
 def index(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        print("success")
-        return render(request,"book/index.html")
+    if login_status == 1:
+        return render(request, "book/index.html")
     else:
-        error = "Username or password incorrect"
-        return render(request, "book/login.html",{"errors":error})
+        return redirect("login")
+
