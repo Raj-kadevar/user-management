f47aea71395d02f2806d3a006b3bdd261b6f2f45
DivyeshP <divyesh.patoriya@trootech.com>
2023-08-03 12:18:14 +0530

    Session
    session functionality added

--- book/views.py
+++ book/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
 from django.views import View
 
-login_status = 0
 
 class LoginView(View):
     def get(self, request, *args, **kwargs):
@@ -13,15 +12,14 @@ class LoginView(View):
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            global login_status
-            login_status = 1
+            request.session['username'] = username
             return HttpResponseRedirect("index")
         else:
             error = "Username or password incorrect"
             return render(request, "book/login.html", {"errors": error})
 
 def index(request):
-    if login_status == 1:
+    if 'username' in request.session:
         return render(request, "book/index.html")
     else:
         return redirect("login")
